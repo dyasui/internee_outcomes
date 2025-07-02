8cda48320b5df6ba434c95d00987b21f65c9b673
dyasui <dyasui@uoregon.edu>
2025-07-02 11:28:48 -0700

    adding to targets workflow

--- R/download-ipums.R
+++ R/download-ipums.R
@@ -1,53 +1,61 @@
 library(ipumsr)
 library(tidyverse)
-ipums_samples <- c("us1940b", "us1950b")
-
-geo_vars <- c("STATEFIP", "STATEICP", "COUNTYICP", "ENUMDIST", "CITY", "URBAN", "FARM")
-dem_vars <-c("SEX", "BIRTHYR", "AGE", "MARST", "CHBORN")
-fam_vars <-c("MOMLOC", "POPLOC", "SPLOC", "NCHILD", "NSIBS", "NFAMS")
-nat_vars <- c("RACE", "BPL", "MBPL", "FBPL", "NATIVITY", "CITIZEN")
-edu_vars <- c("EDUC", "SCHOOL", "HIGRADE")
-emp_vars <- c("EMPSTAT", "LABFORCE", "CLASSWKR",
-                 "OCC", "OCC1950", "IND", "IND1950",
-                 "WKSWORK1", "HRSWORK1", "DURUNEMP")
-inc_vars <- c("INCTOT", "INCWAGE", "INCBUSFM", "INCOTHER", "OCCSCORE")
-
-# list all variables to be downloaded
-fcount_vars <- list(
-  geo_vars,
-  dem_vars,
-  nat_vars,
-  edu_vars,
-  emp_vars,
-  inc_vars,
-  "MIGRATE5", "MIGPLAC5", "MIGCOUNTYICP5",
-  "SLWT"
-) |>
-  unlist() |>
-  as.list()
 
 # download data using IPUMS API
 define_ipums_extract <- function(samples = c("us1940b","us1950b"),
-                                 vars,
                                  description = "Full count census data for 1940 and 1950") {
+
+  geo_vars <- c("STATEFIP", "STATEICP", "COUNTYICP", "ENUMDIST", "CITY", "URBAN", "FARM")
+  dem_vars <-c("SEX", "BIRTHYR", "AGE", "MARST", "CHBORN")
+  fam_vars <-c("MOMLOC", "POPLOC", "SPLOC", "NCHILD", "NSIBS", "NFAMS")
+  nat_vars <- c("RACE", "BPL", "MBPL", "FBPL", "NATIVITY", "CITIZEN")
+  edu_vars <- c("EDUC", "SCHOOL", "HIGRADE")
+  emp_vars <- c("EMPSTAT", "LABFORCE", "CLASSWKR",
+                   "OCC", "OCC1950", "IND", "IND1950",
+                   "WKSWORK1", "HRSWORK1", "DURUNEMP")
+  inc_vars <- c("INCTOT", "INCWAGE", "INCBUSFM", "INCOTHER", "OCCSCORE")
+  
+  # list all variables to be downloaded
+  fcount_vars <- list(
+    geo_vars,
+    dem_vars,
+    nat_vars,
+    edu_vars,
+    emp_vars,
+    inc_vars,
+    "MIGRATE5", "MIGPLAC5", "MIGCOUNTYICP5",
+    "SLWT"
+  ) |>
+    unlist() |>
+    as.list()
+
   define_extract_micro(
     collection = "usa",
     description = description,
     samples = samples,
-    variables = vars
+    variables = fcount_vars
   )
 }
 
 submit_and_download_extract <- function(extract_def, download_dir = "data/fullcount_census") {
-  existing <- get_extract_history("usa")
-  match <- purrr::detect(existing, ~ identical(.x$definition, extract_def$definition))
-
-  if (!is.null(match) && match$status == "completed") {
-    return(download_extract(match, download_dir))
+  # 1. Check for existing downloads
+  existing_files <- list.files(path = download_dir, pattern = "^usa_\\d+\\.xml$", full.names = TRUE)
+  
+  if (length(existing_files) > 0) {
+    # Assume the latest by modification time is what we want
+    latest_file <- existing_files[which.max(file.info(existing_files)$mtime)]
+    message("Using existing extract file: ", latest_file)
+    return(latest_file)
   }
-
+  
+  # 2. If no existing extract, submit and download
   submitted <- submit_extract(extract_def)
   ready <- wait_for_extract(submitted)
   download_extract(ready, download_dir)
-  return(ready)
+  
+  # 3. Construct path of new download
+  new_file <- file.path(download_dir, sprintf("usa_%05d.xml", ready$description$number))
+  message("Downloaded new extract file: ", new_file)
+  
+  return(new_file)
 }
--- _targets.R
+++ _targets.R
@@ -7,7 +7,8 @@ suppressPackageStartupMessages(library(dplyr))
 # Set target options:
 tar_option_set(
   packages = c("tidyverse", "ipumsr", "haven", "fixest", "duckdb", "dbplyr"), # Packages that your targets need for their tasks.
-  format = "fst_tbl", # Optionally set the default storage format. qs is fast.
+  format = "rds", # Optionally set the default storage format. qs is fast.
+  error = "null" # produce a result even if the target errored out.
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -16,8 +17,29 @@ tar_source()
 
 # Target list describes workflow
 list(
-  # Raw data files
+  # Process internment camp data
   tar_target(form26_raw, "data/WRA.FORM26.PU.txt", format = "file"),
   tar_target(wra_addr, "data/WRA_prev_address.csv", format = "file"),
-  tar_target(wra_data, compile_WRA(form26_raw, wra_addr))
+  tar_target(wra_data, compile_WRA(form26_raw, wra_addr)),
+
+  # download IPUMS data and save extract locations
+  tar_target(extract_def, define_ipums_extract()),
+  tar_target(
+    extract_ready,
+    submit_and_download_extract(extract_def, download_dir = "data/fullcount_census/")
+  ),
+  tar_target(ddi_path, extract_ready, format = "file"),
+
+  # Read ipums microdata to memory
+  tar_target(ipums_ddi, read_ipums_ddi(ddi_path)),
+
+  # Write MLP crosswalks to local database to save on memory
+  tar_target(
+    cw_csv,
+    "data/mlp_1940_1950_v1_2_csv/mlp_1940_1950_v1.2.csv",
+    format = "file"
+  ),
+  tar_target(cw_db, write_cw_db(db_path = "data/ipums_db.duckdb", cw_file = cw_csv, tbl_name = "mlp_crosswalks"))
+  
+  
 )
