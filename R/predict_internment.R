1455a087f9a4f09d27d40a98efa8fc9ba8a63ffc
dyasui <dyasui@uoregon.edu>
2025-07-18 15:04:51 -0700

    Rewrote calc_int_proportion to take ddi as 1940 census data input

--- R/predict_internment.R
+++ R/predict_internment.R
@@ -1,49 +1,70 @@
-calc_int_proportion <- function(internees,
-                                db = "data/ipums_db.duckdb",
-                                table = "ipums_microdata",
-                                by = c("STATEFIP", "COUNTYICP")
-                                ) {
+calc_int_proportion <- function(wra_data, ddi,
+                                by = c("STATEFIP", "COUNTYICP", "RACE"),
+                                chunk_size = 1e6,
+                                label = "intern_p") {
   library(tidyverse)
   library(dbplyr)
   library(dbplyr)
   library(duckdb)
 
-  # collect groups from 1940 census
-  con <- dbConnect(duckdb(), dbdir = db)
-  linking_vars <- c("STATEFIP", "COUNTYICP", "RACE", "SEX", "BIRTHYR", "BPL")
+  by <- c("STATEFIP", "COUNTYICP", "RACE", "SEX", "BIRTHYR", "BPL")
+  mutate_vars <- list() # variables to be mutated if present
+  groups <- by
+  
+  if ("BPL" %in% by) {
+    mutate_vars <- append(mutate_vars, list(bpl_grp = expr(group_bpl(BPL))))
+    groups <- setdiff(groups, "BPL")
+  }
+  
+  if ("BIRTHYR" %in% by) {
+    mutate_vars <- append(mutate_vars, list(byr_grp = expr(group_birthyr(BIRTHYR))))
+    groups <- setdiff(groups, "BIRTHYR")
+  }
+  
+  groups <- c(groups, names(mutate_vars))
+  
+  callback <- function(x, pos) {
+    if (length(mutate_vars) > 0) {
+      x <- x |> mutate(!!!mutate_vars)
+    }
+    result <- x |> count(across(all_of(c(groups, "YEAR"))))
+    print(result) # for debuging
+    return(result)
+  }
 
-  pop_grp <- tbl(con, table) |>
-    filter(YEAR==1940) |>
-    # add cohort variable to census
-    collect() |>
-    mutate(
-      bpl_grp = group_bpl(BPL),
-      byr_grp = group_birthyr(BIRTHYR)
-    ) |> 
-    select(unname(by)) |>
-    group_by(across(all_of(unname(by)))) |>
-    summarise(n_j=n(), .groups = "drop") 
-  dbDisconnect(con)
+  chunks <- read_ipums_micro_chunked(
+    ddi = ddi,
+    callback = IpumsDataFrameCallback$new(callback),
+    chunk_size = chunk_size,
+    vars = c(by, "YEAR")
+  )
+
+  pop_grp <- chunks |>
+    filter(YEAR==1940) |> 
+    group_by(across(all_of(groups))) |>
+    summarise(n_census = sum(n), .groups = "drop") 
 
   # Count by group in internee data
-  int_grp <- internees |>
-    filter(race==5) |>
+  int_grp <- wra_data |>
     rename_with(toupper, c("race", "sex", "birthyr", "bpl", "bpl_pop", "bpl_mom", "yrimmig", "nativity", "degfield", "educ")) |>
-    group_by(across(all_of(unname(by)))) |>
-    summarise(n_i = n(), .groups = "drop")
+    mutate(!!!mutate_vars) |> 
+    ## filter(RACE==5) |>
+    group_by(across(all_of(unname(groups)))) |>
+    summarise(n_interned = n(), .groups = "drop")
 
   # Join and calculate fraction
   proportions <- pop_grp |>
-    full_join(int_grp, by = by) |>
+    full_join(int_grp, by = groups) |>
     mutate(
-      n_i = replace_na(n_i, 0),
-      p_ij = n_i / n_j
-    )
+      n_interned = replace_na(n_interned, 0),
+      pr_interned = n_interned / n_census
+    ) |>
+    rename(!!label := pr_interned)
 
   return(proportions)
 }
 
-predict_internment <- function(df, options = c("full", "county", "Arellano-Bover"), custom_vars = NULL, wra_data) {
+predict_internment <- function(df, options = c("full", "county", "Arellano-Bover"),  wra_data) {
 
   df <- df |>
     mutate(
